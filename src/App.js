6d70e2a77b01b5b3700438031293c8b65b820562
sofiaould <sofiaouldabderrahmane@yahoo.fr>
2021-06-24 21:27:49 +0200

    iteration1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
+/// contacts
+let bestContact = contacts.splice(0, 5);
+
+///// iteration1//////
+
+
+
 
 class App extends React.Component {
+
+  state = {
+    Contacts:bestContact 
+  }
   render() {
-    return (
-      <div className="App" >
-      </div>
-    );
+   return(
+    <table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+    {bestContact.map(contact => (
+      <tr key={contact.id}>
+        <td> <img src={contact.pictureUrl}/></td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity}</td>
+      </tr>
+    ))}
+    </tbody>
+  </table>
+   )
+    ;
   }
 }
 
